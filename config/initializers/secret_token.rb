8917a9dc84bbdbbe9fe72a16a4b1f44c03cd76cf
Student <student@CA-iMac-03.local>
2012-11-09 17:05:06 -0600

    first commit as me

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThePitch::Application.config.secret_token = '99fc4493d705d79a7b2b724712f7e4518d5a3cc693cb1ea1733bbb8ec7142be8cd63ac18b3fd5df123ce4ffbcd3734e93e959048f5e297c0c1f55492b16f9115'
