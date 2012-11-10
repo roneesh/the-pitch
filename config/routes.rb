640dba4c06ff2fab9e3efb5c7a86702e2a938243
Peter-Jan Celis <pj@judge.me>
2012-11-10 00:20:10 -0600

    fixed user views

--- config/routes.rb
+++ config/routes.rb
@@ -4,22 +4,19 @@ get '/login' => 'sessions#new', :as => 'login'
 post '/sessions/create' => 'sessions#create'
 get '/logout' => 'sessions#destroy', :as => 'logout'
 
-
-get '/users', controller: 'Users', action: 'index', as: 'users'
-  
-get '/user/new', controller: 'Users', action: 'new'
-  
-post '/user', controller: 'Users', action: 'create'
-  
-get '/user/:id', controller: 'Users', action: 'show'
-  
-get '/user/:id/edit', controller: 'Users', action: 'edit', as: 'edit_user'
- 
-put '/user/:id', controller: 'Users', action: 'update'
-  
-delete 'user/:id', controller: 'Users', action: 'destroy'
+get 'userlogin' => 'sessions#newuser', :as => 'userlogin'
 
 
+
+# get '/users', controller: 'Users', action: 'index', as: 'users'
+# post '/users', controller: 'Users', action: 'create'
+# get '/users/new' => 'Users#new'
+# get '/users/:id/edit', controller: 'Users', action: 'edit', as: 'edit_user'
+# get '/users/:id', controller: 'Users', action: 'show', :as => 'user'
+# 
+# put '/users/:id', controller: 'Users', action: 'update'
+# delete '/users/:id', controller: 'Users', action: 'destroy'
+resources :users
 
 get '/pitches/new' => 'Pitch#new', :as => 'new_pitch'
 post '/pitches' => 'Pitch#create'
@@ -38,14 +35,15 @@ get '/employers/:id/edit' => 'employers#edit', :as => 'edit'
 put '/employers/:id' => 'employers#update', :as => 'update'
 delete '/employers/:id' => 'employers#destroy', :as => 'delete'
 
+
 get '/jobs' => 'jobs#index', :as => 'jobs'
 get '/jobs/new' => 'jobs#new', :as => 'new_job'
 post '/jobs' => 'jobs#create'
 get '/jobs/:id' => 'jobs#show', :as => 'job'
 get '/jobs/:id/edit' => 'jobs#edit', :as => 'edit_job'
 put '/jobs/:id' => 'jobs#update', :as => 'update_job'
-
 delete '/jobs/:id' => 'jobs#destroy', :as => 'destroy_job'
+
 end
 
 
