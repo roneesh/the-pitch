92de2f3bfcf98db090180a95e1f3a43f6bf77cc7
Roneesh Vashisht <roneesh@gmail.com>
2012-12-04 23:05:11 -0600

    adding s3 storage for avatar and resume

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -11,9 +11,12 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # include Sprockets::Helpers::IsolatedHelper
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  # storage :file
+    storage :fog
   # storage :s3
+
+  include CarrierWave::MimeTypes
+  process :set_content_type
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
